9d97d02455e9f54c7a0ee531aa90751dcbbce41e
Daniela Brown <daniimbrown@gmail.com>
2025-02-11 21:30:20 -0300

    realizamos app del clima

--- js/pokemon.js
+++ js/pokemon.js
@@ -5,11 +5,11 @@ const URL_API = 'https://pokeapi.co/api/v2/'
 function obtenerPokemones() {
     //1. Hacer una petición al servidor con un método asíncrono con axios.
     axios.get(`${URL_API}/pokemon`).then(response => {
-        console.log(response)
+        // console.log(response)
 
         //2.Guardar la respuesta en la variable pokemones.
         pokemones = response.data.results
-        console.log(pokemones)
+        // console.log(pokemones)
         //3. Pintar los pokemones en el html.
         pintarPokemones(pokemones)
     })
@@ -37,11 +37,23 @@ function pintarPokemones(arrayPokemones) {
         // ${pokemon.name} <a href="/pages/detail.html?pokemon=${ID}" class="btn btn-primary btn-sm">Ver</a> </li>`
 
         const listItem = document.createElement('li')
-        console.dir(listItem)
+        // console.dir(listItem)
 
         listItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center')
 
         listItem.innerText = pokemon.name
+
+        const aHTML = document.createElement('a') //<a></a>
+        aHTML.classList.add('btn', 'btn-primary', 'btn-sm') //<a class="btn btn-primary btn-sm"></a>
+        aHTML.innerText = 'Ver' //<a class="btn btn-primary btn-sm">Ver</a>
+
+        aHTML.href = `/pages/detail.html?pokemon=${ID}` //<a class="btn btn-primary btn-sm" href="/pages/detail.html?pokemon=${ID}">Ver</a>. 
+
+        //Se puede setear el atributo href de la siguiente forma, y se puede usar para setear cualquier atributo.
+        // aHTML.setAttribute('href', `/pages/detail.html?pokemon=${ID}`) 
+
+        aHTML.style.backgroundColor = 'red' //Para crear estilos.
+        listItem.appendChild(aHTML) //<li> <a>Ver</a> </li>
 
         pokemonListHTML.appendChild(listItem)
 
